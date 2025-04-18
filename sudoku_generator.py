6ead9cae157b21e492e78937f09f2cdbe360d082
Gabriel Dos Santos <Gabedossantos2003@gmail.com>
2025-04-18 18:26:31 -0400

    changes

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -27,7 +27,6 @@ class SudokuGenerator:
     '''
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
-
         self.removed_cells = removed_cells
         self.board = self.get_board()
         self.box_length = math.sqrt(row_length)
